9001c735d318d40cdb02f3dc9013f58290f1e1ea
Jersey <marcjerseycastro@gmail.com>
2024-05-02 19:12:01 +0800

    trashes

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -36,9 +36,10 @@ Partial Class Form1
 		'
 		Me.Label1.AutoSize = True
 		Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.Label1.Location = New System.Drawing.Point(317, 9)
+		Me.Label1.Location = New System.Drawing.Point(238, 7)
+		Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
 		Me.Label1.Name = "Label1"
-		Me.Label1.Size = New System.Drawing.Size(191, 58)
+		Me.Label1.Size = New System.Drawing.Size(153, 46)
 		Me.Label1.TabIndex = 0
 		Me.Label1.Text = "NOM U"
 		'
@@ -46,9 +47,10 @@ Partial Class Form1
 		'
 		Me.Label2.AutoSize = True
 		Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.Label2.Location = New System.Drawing.Point(290, 78)
+		Me.Label2.Location = New System.Drawing.Point(218, 63)
+		Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
 		Me.Label2.Name = "Label2"
-		Me.Label2.Size = New System.Drawing.Size(232, 39)
+		Me.Label2.Size = New System.Drawing.Size(186, 31)
 		Me.Label2.TabIndex = 1
 		Me.Label2.Text = "Student Portal"
 		'
@@ -56,9 +58,10 @@ Partial Class Form1
 		'
 		Me.Label3.AutoSize = True
 		Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.Label3.Location = New System.Drawing.Point(92, 184)
+		Me.Label3.Location = New System.Drawing.Point(69, 150)
+		Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
 		Me.Label3.Name = "Label3"
-		Me.Label3.Size = New System.Drawing.Size(148, 29)
+		Me.Label3.Size = New System.Drawing.Size(116, 25)
 		Me.Label3.TabIndex = 2
 		Me.Label3.Text = "User Name:"
 		'
@@ -66,35 +69,39 @@ Partial Class Form1
 		'
 		Me.Label4.AutoSize = True
 		Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.Label4.Location = New System.Drawing.Point(109, 234)
+		Me.Label4.Location = New System.Drawing.Point(82, 190)
+		Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
 		Me.Label4.Name = "Label4"
-		Me.Label4.Size = New System.Drawing.Size(131, 29)
+		Me.Label4.Size = New System.Drawing.Size(104, 25)
 		Me.Label4.TabIndex = 3
 		Me.Label4.Text = "Password:"
 		'
 		'TxtUserName
 		'
-		Me.TxtUserName.Location = New System.Drawing.Point(256, 169)
+		Me.TxtUserName.Location = New System.Drawing.Point(192, 137)
+		Me.TxtUserName.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
 		Me.TxtUserName.Multiline = True
 		Me.TxtUserName.Name = "TxtUserName"
-		Me.TxtUserName.Size = New System.Drawing.Size(373, 44)
+		Me.TxtUserName.Size = New System.Drawing.Size(281, 36)
 		Me.TxtUserName.TabIndex = 4
 		'
 		'txtPassword
 		'
-		Me.txtPassword.Location = New System.Drawing.Point(256, 234)
+		Me.txtPassword.Location = New System.Drawing.Point(192, 190)
+		Me.txtPassword.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
 		Me.txtPassword.Multiline = True
 		Me.txtPassword.Name = "txtPassword"
-		Me.txtPassword.Size = New System.Drawing.Size(373, 44)
+		Me.txtPassword.Size = New System.Drawing.Size(281, 36)
 		Me.txtPassword.TabIndex = 5
 		'
 		'BtnLogIn
 		'
 		Me.BtnLogIn.AccessibleName = ""
 		Me.BtnLogIn.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.BtnLogIn.Location = New System.Drawing.Point(82, 405)
+		Me.BtnLogIn.Location = New System.Drawing.Point(62, 329)
+		Me.BtnLogIn.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
 		Me.BtnLogIn.Name = "BtnLogIn"
-		Me.BtnLogIn.Size = New System.Drawing.Size(216, 94)
+		Me.BtnLogIn.Size = New System.Drawing.Size(162, 76)
 		Me.BtnLogIn.TabIndex = 6
 		Me.BtnLogIn.Text = "LOG IN"
 		Me.BtnLogIn.UseVisualStyleBackColor = True
@@ -103,19 +110,20 @@ Partial Class Form1
 		'
 		Me.btnAddStudent.AccessibleName = ""
 		Me.btnAddStudent.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.btnAddStudent.Location = New System.Drawing.Point(327, 405)
+		Me.btnAddStudent.Location = New System.Drawing.Point(245, 329)
+		Me.btnAddStudent.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
 		Me.btnAddStudent.Name = "btnAddStudent"
-		Me.btnAddStudent.Size = New System.Drawing.Size(389, 94)
+		Me.btnAddStudent.Size = New System.Drawing.Size(292, 76)
 		Me.btnAddStudent.TabIndex = 11
 		Me.btnAddStudent.Text = "ADD STUDENT"
 		Me.btnAddStudent.UseVisualStyleBackColor = True
 		'
 		'Form1
 		'
-		Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
 		Me.BackColor = System.Drawing.SystemColors.ActiveCaption
-		Me.ClientSize = New System.Drawing.Size(807, 607)
+		Me.ClientSize = New System.Drawing.Size(605, 493)
 		Me.Controls.Add(Me.btnAddStudent)
 		Me.Controls.Add(Me.BtnLogIn)
 		Me.Controls.Add(Me.txtPassword)
@@ -125,6 +133,7 @@ Partial Class Form1
 		Me.Controls.Add(Me.Label2)
 		Me.Controls.Add(Me.Label1)
 		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+		Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
 		Me.Name = "Form1"
 		Me.Text = "Form1"
 		Me.ResumeLayout(False)
--- Form1.vb
+++ Form1.vb
@@ -116,4 +116,8 @@
 		Form3.Show()
 		Me.Hide()
 	End Sub
+
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+	End Sub
 End Class
